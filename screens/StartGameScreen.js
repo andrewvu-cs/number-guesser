d9ba44d72cd2a30a0e06672773a2ace422b75c67
andrewvu-cs <andrewvu.cs@gmail.com>
2019-10-15 11:12:35 -0700

    furthered game functionality, provided handlers and some validation for user input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,29 +1,79 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard
+} from "react-native";
 
 import Card from "../components/Card";
 import Colors from "../constants/colors";
-import Input from '../components/Input';
+import Input from "../components/Input";
 
 const { primary, secondary } = Colors;
 
 const StartGameScreen = props => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+
+  const numberInputHandler = inputText => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  };
+
+  const resetInputHandler = () => {
+    setEnteredValue('');
+    setConfirmed(false);
+  }
+
+  const confirmInputHandler = () => {
+      const chosenNumber = parseInt(enteredValue);
+      if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99){
+        return;
+      }
+      setConfirmed(true);
+      setSelectedNumber(chosenNumber);
+      setEnteredValue('');
+  }
+
+  let confirmedOutput;
+
+  if (confirmed) {
+    confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+  }
+  // blurOnSubmit is an android thing
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>The Game Screen!</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Select a Number!</Text>
-        <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType="numeric" maxLength={2}/>
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Button title="Reset" onPress={() => {}} color={secondary} />
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+    }}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>The Game Screen!</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a Number!</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="numeric"
+            maxLength={2}
+            onChangeText={numberInputHandler}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button title="Reset" onPress={resetInputHandler} color={secondary} />
+            </View>
+            <View style={styles.button}>
+              <Button title="Confirm" onPress={confirmInputHandler} color={primary} />
+            </View>
           </View>
-          <View style={styles.button}>
-            <Button title="Confirm" onPress={() => {}} color={primary} />
-          </View>
-        </View>
-      </Card>
-    </View>
+        </Card>
+        {confirmedOutput}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
